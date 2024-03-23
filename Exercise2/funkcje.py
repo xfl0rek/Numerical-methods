04cd3461fcf2c90cf543f8e87e94b13d652090f3
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-03-23 12:58:48 +0100

    U and D matrix.

--- Exercise2/funkcje.py
+++ Exercise2/funkcje.py
@@ -9,3 +9,20 @@ def L(macierz):
         for j in range(i + 1, len(nowa_macierz[i])):
             nowa_macierz[i][j] = 0
     return nowa_macierz
+
+
+def U(macierz):
+    nowa_macierz = [wiersz[:] for wiersz in macierz]
+    for i in range(len(nowa_macierz)):
+        for j in range(len(nowa_macierz)):
+            if j < i:
+                nowa_macierz[i][j] = 0
+    return nowa_macierz
+
+
+def D(macierz):
+    nowa_macierz = [wiersz[:] for wiersz in macierz]
+    _L = L(nowa_macierz)
+    _U = U(_L)
+    _D = _U
+    return _D
--- Exercise2/main.py
+++ Exercise2/main.py
@@ -7,9 +7,15 @@ macierz2 = [[3, 3, 1, 2], [2, 5, 7, 3], [1, 2, 1, 4], [1, 2, 3, 4]]
 
 def main():
     f.wypisz_macierz(macierz, 3)
+    print()
     dupa = f.L(macierz2)
     f.wypisz_macierz(dupa, 4)
-    print(dupa)
+    print()
+    dupa2 = f.U(macierz2)
+    f.wypisz_macierz(dupa2, 4)
+    print()
+    dupa3 = f.D(macierz2)
+    f.wypisz_macierz(dupa3, 4)
 
 
 if __name__ == "__main__":
