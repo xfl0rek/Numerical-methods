0d34218f432b1795789e693fcaf3c41560265235
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-02 12:15:13 +0200

    Menu update

--- Exercise2/main.py
+++ Exercise2/main.py
@@ -30,9 +30,48 @@ def main():
                 wynik = fun.gauss_seidel(macierz, b, None, None, epsilon)
                 print(wynik)
             else:
-                print("Niepoprawny wybor")
+                print("Niepoprawny wybór.")
         elif wybor == "2":
-            print("dupa")
+            print("Podaj ile równań chcesz rozwiązać:")
+            ile = int(input())
+            print("Wybierz kryterium zatrzymania")
+            print("1. Liczba iteracji")
+            print("2. Dokładność")
+            wybor2 = input()
+
+            if wybor2 == "1":
+                print("Podaj maksymalna liczbe iteracji:")
+                max_iter = int(input())
+                wybrane_rownania = []
+                wyniki = []
+                for i in range(ile):
+                    print(f"Podaj nazwę {i + 1} rownania:")
+                    x = input()
+                    wybrane_rownania.append(x)
+
+                for i in range(len(wybrane_rownania)):
+                    przyklad = wybrane_rownania[i]
+                    macierz, b = op.wczytaj_macierz(przyklad)
+                    wynik = fun.gauss_seidel(macierz, b, None, max_iter, None)
+                    wyniki.append(wynik)
+                print(wyniki)
+            elif wybor2 == "2":
+                print("Podaj dokładność:")
+                epsilon = float(input())
+                wybrane_rownania = []
+                wyniki = []
+                for i in range(ile):
+                    print(f"Podaj nazwę {i + 1} rownania:")
+                    x = input()
+                    wybrane_rownania.append(x)
+                for i in range(len(wybrane_rownania)):
+                    przyklad = wybrane_rownania[i]
+                    macierz, b = op.wczytaj_macierz(przyklad)
+                    wynik = fun.gauss_seidel(macierz, b, None, None, epsilon)
+                    wyniki.append(wynik)
+                print(wyniki)
+            else:
+                print("Niepoprawny wybór.")
         else:
             menu = False
 
