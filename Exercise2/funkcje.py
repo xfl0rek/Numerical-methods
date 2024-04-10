958aa6dd6f51134ca1271f96cfca6e9e622d9a2e
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-10 10:42:20 +0200

    removed unused method.

--- Exercise2/funkcje.py
+++ Exercise2/funkcje.py
@@ -1,11 +1,6 @@
 import numpy as np
 
 
-def wypisz_macierz(macierz, rozmiar_macierzy):
-    for i in range(rozmiar_macierzy):
-        print(macierz[i])
-
-
 def oblicz_wyznacznik(matrix):
     wyznacznik = np.linalg.det(matrix)
     return wyznacznik
