9ce7c2f218c3fe7a7d2fe92ae7a2a4f50d43f1d4
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-19 11:47:12 +0200

    Separated plotting logic to different file.

--- Exercise3/LagrangeInterpolation.py
+++ Exercise3/LagrangeInterpolation.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 class LagrangeInterpolation:
     def __init__(self, nodes, values):
         self.nodes = nodes
@@ -16,26 +12,3 @@ class LagrangeInterpolation:
                     term *= (x - self.nodes[j]) / (self.nodes[i] - self.nodes[j])
             result += term
         return result
-
-
-class InterpolationPlotter:
-    def __init__(self, original_function, interpolation_function, nodes, values):
-        self.original_function = original_function
-        self.interpolation_function = interpolation_function
-        self.nodes = nodes
-        self.values = values
-
-    def plot(self):
-        x_values = np.linspace(min(self.nodes), max(self.nodes), 1000)
-        original_values = self.original_function(x_values)
-        interpolation_values = [self.interpolation_function.interpolate(x) for x in x_values]
-
-        plt.plot(x_values, original_values, label="Original Function")
-        plt.plot(x_values, interpolation_values, label="Interpolation")
-        plt.scatter(self.nodes, self.values, color='red', label="Interpolation Nodes")
-        plt.legend()
-        plt.xlabel("x")
-        plt.ylabel("f(x)")
-        plt.title("Interpolation using Lagrange's method")
-        plt.grid(True)
-        plt.show()
--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,6 +1,7 @@
 import Functions as f
 import LagrangeInterpolation
 import LoadData
+import InterpolationPlotter
 
 
 def main():
@@ -10,7 +11,7 @@ def main():
 if __name__ == "__main__":
     nodes, values = LoadData.load_data("DataFiles/data1.txt")
     lagrange_interpolation = LagrangeInterpolation.LagrangeInterpolation(nodes, values)
-    plotter = LagrangeInterpolation.InterpolationPlotter(f.composite, lagrange_interpolation, nodes, values)
+    plotter = InterpolationPlotter.InterpolationPlotter(f.composite, lagrange_interpolation, nodes, values)
     plotter.plot()
 
     main()
