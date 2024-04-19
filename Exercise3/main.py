c18ee43018afc4696b3071971708bef3384ab8bb
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-19 13:27:59 +0200

    Code changes.

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,30 +1,27 @@
 import Functions as f
 import LagrangeInterpolation
-import LoadData
 import InterpolationPlotter
 
 
 def main():
-    function_type = input("Choose the interpolated function (1 - linear, 2 - |x|, 3 - polynomial, 4 - trigonometric, 5 - composite): ")
+    function_type = input(
+        "Choose the interpolated function (1 - linear, 2 - |x|, 3 - polynomial, 4 - trigonometric, 5 - composite): ")
     if function_type not in ["1", "2", "3", "4", "5"]:
         print("Invalid function type selected.")
         return
 
-    filename = input("Enter the filename with data: ")
+    start_interval = float(input("Enter the start of the interpolation interval: "))
+    end_interval = float(input("Enter the end of the interpolation interval: "))
 
-    try:
-        nodes, values = LoadData.load_data(filename)
-    except FileNotFoundError:
-        print("File not found.")
-        return
+    num_nodes = int(input("Enter the number of interpolation nodes: "))
+
+    nodes = [start_interval + (end_interval - start_interval) * i / (num_nodes - 1) for i in range(num_nodes)]
+    values = [f.function_value(function_type)(node) for node in nodes]
 
     interpolation_function = f.function_value(function_type)
 
-    if interpolation_function is None:
-        print("Invalid function type.")
-        return
-
     lagrange_interpolation = LagrangeInterpolation.LagrangeInterpolation(nodes, values, interpolation_function)
+
     plotter = InterpolationPlotter.InterpolationPlotter(interpolation_function, lagrange_interpolation, nodes, values)
     plotter.plot()
 
