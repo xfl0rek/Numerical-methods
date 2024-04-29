95784e31b6a1f7c78867fc2ee2cdfe799ecfefbb
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-04-29 18:52:41 +0200

    Code fix.

--- Exercise4/CompositeNewtonCotes.py
+++ Exercise4/CompositeNewtonCotes.py
@@ -9,13 +9,11 @@ class CompositeNewtonCotes:
         for _ in range(max_iterations):
             h = (b - a) / n
             result = 0
-            x0 = a
 
             for i in range(n):
                 x1 = a + i * h
                 x2 = a + (i + 1) * h
-                result += self.simpson(x0, x2)
-                x0 = x1
+                result += self.simpson(x1, x2)
 
             if prev_result is not None and abs(result - prev_result) < tolerance:
                 break
@@ -27,4 +25,4 @@ class CompositeNewtonCotes:
 
     def simpson(self, x1, x2):
         h = (x2 - x1) / 2
-        return (h / 2) * (self.function(x1) + 4 * self.function((x1 + x2) / 2) + self.function(x2)) / 3
+        return (h / 3) * (self.function(x1) + 4 * self.function((x1 + x2) / 2) + self.function(x2))
