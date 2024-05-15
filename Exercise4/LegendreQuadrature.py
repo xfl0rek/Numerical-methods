084f513a81a09a7c41353f2299ce152a7d6eb438
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-05-15 11:10:23 +0200

    Small code fix.

--- Exercise4/LegendreQuadrature.py
+++ Exercise4/LegendreQuadrature.py
@@ -4,15 +4,15 @@ from scipy.special import roots_legendre
 class LegendreQuadrature:
     def __init__(self, function, nodes=5):
         self.function = function
-        self.degree = nodes
+        self.nodes = nodes
 
     def integrate(self, a, b):
         x_mapped = lambda t: (b - a) / 2 * t + (b + a) / 2
 
-        nodes, weights = roots_legendre(self.degree)
+        nodes, weights = roots_legendre(self.nodes)
 
         integral = 0
-        for i in range(self.degree):
+        for i in range(self.nodes):
             integral += weights[i] * self.function(x_mapped(nodes[i]))
 
         integral *= (b - a) / 2
